45ba1faf825fd3acb8663662a551d91adf19806a
nmnikhil <nmnikhil726@gmail.com>
2023-10-10 15:07:23 +0530

    Handsonlist 2

--- HandsOnList2/26.c
+++ HandsOnList2/26.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/stat.h>
@@ -7,20 +6,41 @@
 #include <sys/stat.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <string.h> // Added for strlen
+
 int main() {
- struct msg {
- long int m_type;
- char message[80];
- } myq;
- key = ftok(".", 'a');
-mqid = msgget(key, 0);
- printf("Enter message type: ");
- scanf("%ld", &myq.m_type);
- printf("Enter message text:");
- scanf("%[^\n]", myq.message);
- size = strlen(myq.message);
- // size + 1 to accommodate terminating character
- msgsnd(mqid, &myq, size + 1, 0);
+    struct msg {
+        long int m_type;
+        char message[80];
+    } myq;
 
+    key_t key; // Declare key
+    int mqid;   // Declare mqid
+    size_t size; // Declare size
 
+    key = ftok(".", 'a');
+
+    mqid = msgget(key, 0666 | IPC_CREAT); // Create or get a message queue
+
+    if (mqid == -1) {
+        perror("msgget");
+        exit(1);
+    }
+
+    printf("Enter message type: ");
+    scanf("%ld", &myq.m_type);
+
+    printf("Enter message text: ");
+    scanf(" %[^\n]", myq.message); // Note the space before %[^\n] to consume leading whitespace
+
+    size = strlen(myq.message);
+
+    // size + 1 to accommodate the terminating null character
+    if (msgsnd(mqid, &myq, size + 1, 0) == -1) {
+        perror("msgsnd");
+        exit(1);
+    }
+
+    return 0;
 }
+
