458c302c842b6fc1f51da810e76f328d82b1f5c2
nmnikhil <nmnikhil726@gmail.com>
2023-10-10 16:23:23 +0530

    question 27th

---
+++ HandsOnList2/27a.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h> // Added for strlen
+
+int main() {
+    struct msg {
+        long int m_type;
+        char message[80];
+    } myq[2];
+
+    key_t key; // Declare key
+    int mqid;   // Declare mqid
+    size_t size; // Declare size
+
+    key = ftok(".", 'a');
+
+    mqid = msgget(key, 0666 | IPC_CREAT); // Create or get a message queue
+
+    if (mqid == -1) {
+        perror("msgget");
+        exit(1);
+    }
+    for (int i =0;i<2;i++){
+    printf("Enter message type: ");
+    scanf("%ld", &myq[i].m_type);
+
+    printf("Enter message text: ");
+    scanf(" %[^\n]", myq[i].message); // Note the space before %[^\n] to consume leading whitespace
+
+    size = strlen(myq[i].message);
+
+    // size + 1 to accommodate the terminating null character
+    if (msgsnd(mqid, &myq[i], size + 1, 0) == -1) {
+        perror("msgsnd");
+        exit(1);
+    }
+}
+    return 0;
+}
---
+++ HandsOnList2/27b.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h> 
+int main() {
+ struct msg {
+ long int m_type;
+ char message[80];
+ } myq[2];
+key_t key;
+int mqid;
+size_t size;
+ key = ftok(".", 'a');
+ mqid = msgget(key, 0);
+ printf("Enter message type: ");
+ scanf(" %ld", &myq[0].m_type);
+     // Receive a message with no flag (blocking)
+    printf("Receiving a message with no flag (blocking)...\n");
+    msgrcv(mqid, &myq, sizeof(myq[0].message), myq[0].m_type, 0);
+    printf("Received message with type %ld: %s\n", myq[0].m_type, myq[0].message);
+ printf("Enter message type: ");
+ scanf(" %ld", &myq[1].m_type);
+int ret1 = msgrcv(mqid, &myq[1], sizeof(myq[1].message), myq[1].m_type, IPC_NOWAIT | MSG_NOERROR);
+
+ if (ret1 ==-1)
+ exit(-1);
+  printf("Receiving a message with IPC_NOWAIT flag (non-blocking)...\n");
+ printf("Message type: %ld\n Message: %s\n", (myq+1)->m_type, myq[1].message);
+ }
